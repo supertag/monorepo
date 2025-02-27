828635eaa61b94f8b8bf5d4f48d7c4c8a6f4c904
pdi10 <pdi1066@naver.com>
2025-02-27 20:10:22 +0900

    prettier 기능을 Eslint fix 기능으로 대체함

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
+﻿import {
+    Counter,
+    Header
+} from '@repo/ui';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import typescriptLogo from '/typescript.svg';
-import { Header, Counter } from '@repo/ui';
 
-const a = 'a';
-let a1 = ('a' == a || 'b' === a || 'c' === a) ? 'a' : 'b';
-if ('a' == a) a1 = 'a';
-var c;
-const value = 'text'.trim().split('\n');('position').toLowerCase();
 
 const App = () => (
 	<div>
 		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" className="logo" alt="Vite logo" a="d" b="b" c="c" d="d" e="e"/>
+			<img src="/vite.svg" className="logo" alt="Vite logo" a="d" b="b" c="c" d="d" e="e" />
 		</a>
-		<a href="https://www.typescriptlang.org/" target="_blank">
-			<img src={typescriptLogo} className="logo vanilla" alt="TypeScript logo"
-			/>
+		<a href="https://www.typescriptlang.org/"
+			target="_blank">
+			<img src={typescriptLogo}
+				className="logo vanilla"
+				alt="TypeScript logo" />
 		</a>
 		<Header title="Hello Web World" />
 		<div className="card">
 			<Counter />
 		</div>
 	</div>
-)
+);
 createRoot(document.getElementById('app')!).render(<App />);
--- packages/ui/components/header.tsx
+++ packages/ui/components/header.tsx
@@ -4,10 +4,8 @@ interface HeaderProps {
 	title: string;
 }
 
-export const Header: React.FC<HeaderProps> = ({ title }) => {
-	return (
-		<header id="header">
-			<h1>{title}</h1>
-		</header>
-	);
-};
+export const Header: React.FC<HeaderProps> = ({ title }) => (
+        <header id="header">
+            <h1>{title}</h1>
+        </header>
+    );
